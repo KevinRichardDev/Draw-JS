9ba9930d51f80dcf8dd88869586c0a5640740e7e
KevinRichardDev <kevin.richard0@orange.fr>
2024-03-04 17:05:39 +0100

    getBoundingClientRect()

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ const canvas = document.getElementById("art");
 const ctx = canvas.getContext("2d");
 
 function getMousePos(e) {
-  
+  const rect = canvas.getBoundingClientRect();
+
 }
 
 canvas.addEventListener("mousedown", (e) => {
   e.preventDefault();
+  getMousePos(e)
 });
 

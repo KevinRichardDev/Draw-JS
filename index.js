5eee0cb3b41fde5e5b35becba678936af0919f22
KevinRichardDev <kevin.richard0@orange.fr>
2024-03-04 17:03:58 +0100

    getMousePos

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const canvas = document.getElementById("art");
 const ctx = canvas.getContext("2d");
 
+function getMousePos(e) {
+  
+}
+
 canvas.addEventListener("mousedown", (e) => {
   e.preventDefault();
 });
+
